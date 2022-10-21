c0118565b45a5661c152ddd6324cbf50ad5cf32e
Fatima Musa <mpatigul@my.centennialcollege.ca>
2022-10-21 14:12:44 -0400

    Setup passport

--- app/app.js
+++ app/app.js
@@ -9,6 +9,20 @@ import path, { dirname } from 'path';
 import { fileURLToPath } from 'url';
 const __dirname = dirname(fileURLToPath(import.meta.url));
 
+// Auth step 1 import moduls
+
+import passport from 'passport';
+import passportLocal from 'passport-local';
+import flash from 'connect-flash';
+
+// Auth step 2 define our Auth strategy
+
+let localStrategy = passportLocal.Strategy;
+
+// Auth step 3 import the user models
+
+import User from './models/user.js';
+
 // Import Mongoose Module
 import mongoose from 'mongoose';
 
@@ -42,11 +56,30 @@ app.use(express.urlencoded({ extended: false}));
 app.use(cookieParser());
 // app.use(express.static(path.join(__dirname,'/client')));
 app.use(express.static(path.join(__dirname,'../public')));
+
+// Auth step 4 -Setup Express Session
 app.use(session({
     secret: Secret,
     saveUninitialized: false, 
     resave: false
 }));
+
+//Auth step 5 setup flash
+
+app.use(flash());
+
+//Auth Step 6 -Initialize Passport and Session
+app.use(passport.initialize());
+app.use(passport.session());
+
+//Step 7 Impliement the Auth Strategy
+
+passport.use(User.createStrategy());
+
+//step 8 setup serializatin and deserialization
+
+passport.serializeUser(User.serializeUser());
+passport.deserializeUser(User.deserializeUser());
 
 // Use Routes
 app.use('/', indexRouter);
